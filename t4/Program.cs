3218420f85a41978b8359bc41be68c5364f219c1
Maxim Kondratiew <maximkondratiew10@mail.ru>
2023-11-24 18:10:09 +0400

    4task

---
+++ t4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Дано натуральное число в диапазоне от 1 до 100 000. Создайте массив, состоящий из цифр этого числа. 
+// Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем. Р
+// Размер массива должен быть равен количеству цифр.
+
+class Program
+{
+    static void Main()
+    {
+        Random rand= new Random();
+        int number = rand.Next(1, 100001);
+        System.Console.WriteLine("Заданное число: "+ number);
+        int numberOfDigits = (int)Math.Log10(number) + 1;
+        
+        int[] digitsArray = new int[numberOfDigits];
+
+        for (int i = 0; i < numberOfDigits; i++)
+        {
+            digitsArray[i] = number % 10;
+            number /= 10;
+        }
+
+        Console.WriteLine("Массив цифр числа в обратном порядке:");
+
+        foreach (int digit in digitsArray)
+        {
+            Console.Write(digit + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
